10e1bdd03a81a93a61153fd1cff0d13ae4420369
pangaofeng@gmail.com <pangaofeng@gmail.com>
2020-10-12 14:47:06 -0400

    update

--- all_feature.py
+++ all_feature.py
@@ -45,32 +45,36 @@ for train_index, test_index in kf.split(Y_4802):
     test_x = []
     
     for fname in ['ppab', 'ppdwt', 'pppse']: # , 'pssmab', 'pssmdwt', 'pssmpse'
-        fdata = X_4802_feature[fname][train_index]
+        fdata = X_4802_feature[fname][train_index]        
+        fdata = (fdata - np.amin(fdata))/(np.amax(fdata) - np.amin(fdata)) * 250        
         train_x.append(fdata)
-        test_x.append(X_4802_feature[fname][test_index])
+        
+        tdata = X_4802_feature[fname][test_index]
+        tdata = (tdata - np.amin(tdata))/(np.amax(tdata) - np.amin(tdata)) * 250 
+        test_x.append(tdata)
         
         input_shape = fdata.shape
         ix = keras.Input(shape=input_shape[1:], dtype = "float32")
 
         x = layers.Conv2D(256, (4,3), activation='relu', 
                          kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(ix)
-        x = layers.Conv2D(128, 3, activation='relu',
-                         kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
+#         x = layers.Conv2D(128, 3, activation='relu',
+#                          kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
         x = layers.MaxPooling2D()(x)
         x = layers.Conv2D(64, 3, activation='relu',
                          kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
         #x = layers.MaxPooling2D()(x)
         x = layers.Conv2D(32, 3, activation='relu',
                          kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
-        x = layers.Conv2D(16, 3, activation='relu',
-                         kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
+#         x = layers.Conv2D(16, 3, activation='relu',
+#                          kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
         outputs = layers.Flatten()(x)
         #outputs = layers.Dense(37, activation='sigmoid')(x)
         model = keras.Model(ix, outputs)
         model_list.append(model)
 
     x = layers.concatenate([m.output for m in model_list])
-    x = layers.Dense(37, activation='sigmoid')(x)
+    x = layers.Dense(37, activation='sigmoid', kernel_regularizer=regularizers.l1_l2(l1=1e-5, l2=1e-4))(x)
     model = keras.Model(inputs=[m.input for m in model_list], outputs = x)
     model.compile("adam", "binary_crossentropy", metrics=["binary_accuracy"])
     
@@ -87,7 +91,7 @@ ap_values = np.array(ap_list).reshape((num_folds,20))
 rl_values = np.array(rl_list).reshape((num_folds,20))
 ce_values = np.array(ce_list).reshape((num_folds,20))
 
-with open('all_4802_res.txt', 'w') as result_file:    
+with open('all_4802_res-250.txt', 'w') as result_file:    
     result_file.write('the ap score is: \n')
     result_file.write(str(ap_values) + '\n')
     result_file.write('max is: {}'.format(np.amax(ap_values, axis = 1)) + '\n')    
---
+++ encoding-large-bi.py
@@ -0,0 +1,152 @@
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow import keras
+from scipy.io import loadmat, savemat
+
+from tensorflow.keras import layers
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+    
+from sklearn.model_selection import KFold
+from sklearn.metrics import label_ranking_average_precision_score as avgprec
+from sklearn.metrics import coverage_error, label_ranking_loss
+
+num_folds = 5
+    
+Y_4802 = loadmat('Y_4802.mat')['Y_4802']
+
+sequence = loadmat('dataset_4802.mat')['Sequence']
+amino_code = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6,
+             'I':7, 'K':8, 'L':9, 'M':10, 'N':11, 'P':12, 
+             'Q':13, 'R':14, 'S':15, 'T':16, 'U':17, 'V':18,
+             'W':19, 'X':20, 'Y':21 }
+
+# extend_one_encode = []
+# for s in sequence:
+#     p_seq = (list(s[0][0]) * 1000)[0:10000]
+#     seq_num = np.array([amino_code[x] for x in p_seq])
+#     tmp = np.zeros((seq_num.size, 22))
+#     tmp[np.arange(seq_num.size), seq_num] = 1
+#     extend_one_encode.append(tmp)
+def preprocess(p_seq, l):
+    seq_num = np.array([amino_code[x] for x in p_seq])
+    tmp = np.zeros((l, 22))
+    s = (100 - seq_num.size)//2
+    e = s + seq_num.size
+    tmp[np.arange(s, e), seq_num] = 1
+    return tmp
+
+def p_split(p_seq, l):
+    result = []
+    s = 0
+    e = s + l
+    while (s + l//2) < len(p_seq):
+        t = p_seq[s:e]
+        if len(t) < l:
+            t = p_seq[-l:]
+        s = s + l//2
+        e = s + l
+        result.append(t)
+    return result
+
+uniform_one_encode = []
+uniform_label = []
+for sq, lb in zip(sequence, Y_4802):
+    seq = sq[0][0]
+    if len(seq) > 100:
+        result = p_split(seq, 100)
+    else:
+        result = [seq]
+    for r in result:
+        uniform_one_encode.append(preprocess(r, 100))
+        uniform_label.append(lb)
+
+    
+X_4802_uni = np.array(uniform_one_encode)
+Y_4802_uni = np.array(uniform_label)
+
+kf = KFold(num_folds, shuffle = True)
+
+ap_list = []
+rl_list = []
+ce_list = []
+
+#with tf.device("cpu:0"):
+for train_index, test_index in kf.split(Y_4802_uni):
+    train_x = X_4802_uni[train_index]
+    train_y = Y_4802_uni[train_index]
+    
+    test_x = X_4802_uni[test_index]
+    test_y = Y_4802_uni[test_index]
+    
+    inputs = keras.Input(shape=(100, 22,), dtype = "float32")
+    x = layers.Bidirectional(layers.LSTM(256, return_sequences = True))(inputs)
+    #x = layers.Bidirectional(layers.LSTM(128, return_sequences = True))(x)
+    x = layers.Bidirectional(layers.LSTM(128, return_sequences = True))(x)
+    
+    x = layers.Reshape((100, 256, 1))(x)
+
+    x = layers.Conv2D(256, (4,3), activation='relu')(x)
+    x = layers.MaxPooling2D()(x)
+    # x = layers.Dropout(.2)(x)
+    x = layers.Conv2D(128, 3, activation='relu',)(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 3, activation='relu',)(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(32, 3, activation='relu',)(x)
+    
+    x = layers.Reshape((-1, 32))(x)
+    x = layers.Bidirectional(layers.LSTM(256))(x)
+    # x = layers.Flatten()(x)
+    outputs = layers.Dense(37, activation='sigmoid')(x)
+    model = keras.Model(inputs, outputs)
+    model.summary()
+
+    model.compile("adam", "binary_crossentropy", metrics=["binary_accuracy", "binary_crossentropy"])
+#     model.fit(train_x, train_y, batch_size=16, epochs=2)#, validation_data=(x_val, y_val))    
+    for i in range(10):
+        model.fit(train_x, train_y, batch_size=32, epochs=3, verbose = 2)
+        pred_y = model.predict(test_x)
+
+        ap_list.append(avgprec(test_y, pred_y))
+        rl_list.append(label_ranking_loss(test_y, pred_y))
+        ce_list.append(coverage_error(test_y, pred_y) - 1)
+        
+        print('ap_list: {}'.format(ap_list))
+        print('rl_list: {}'.format(rl_list))
+        print('ce_list: {}'.format(ce_list))
+    
+ap_values = np.array(ap_list).reshape((5,10))
+rl_values = np.array(rl_list).reshape((5,10))
+ce_values = np.array(ce_list).reshape((5,10))
+    
+with open('encoding_4802_lrg_bi.txt', 'w') as result_file:    
+    result_file.write('the ap score is: \n')
+    result_file.write(str(ap_values) + '\n')
+    result_file.write('max is: {}'.format(np.amax(ap_values, axis = 1)) + '\n')    
+    result_file.write('k-fold is: {}'.format(np.average(ap_values, axis = 0)) + '\n')
+    result_file.write('k-fold max is: {}'.format(np.amax(np.average(ap_values, axis = 0))) + '\n')
+
+    result_file.write('the rl score is: \n')
+    result_file.write(str(rl_values) + '\n')
+    result_file.write('min is: {}'.format(np.amin(rl_values, axis = 1)) + '\n')    
+    result_file.write('k-fold is: {}'.format(np.average(rl_values, axis = 0)) + '\n')
+    result_file.write('k-fold min is: {}'.format(np.amin(np.average(rl_values, axis = 0))) + '\n')
+
+    result_file.write('the ce score is: \n')
+    result_file.write(str(ce_values) + '\n')
+    result_file.write('min is: {}'.format(np.amin(ce_values, axis = 1)) + '\n')    
+    result_file.write('k-fold is: {}'.format(np.average(ce_values, axis = 0)) + '\n')
+    result_file.write('k-fold min is: {}'.format(np.amin(np.average(ce_values, axis = 0))) + '\n')
